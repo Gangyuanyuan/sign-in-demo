d5b1669af80d20aece6053b44f86f73cc7737b58
Gangyuan <296671396@qq.com>
2019-05-31 22:49:27 +0800

    用jQuery实现ajax

--- main.js
+++ main.js
@@ -1,16 +1,64 @@
-myButton.addEventListener('click', (e)=>{
-    let request = new XMLHttpRequest()
-    request.open('get', 'https://jack.com:8002/xxx') // 初始化request
-    request.send()
-    request.onreadystatechange = ()=>{
-      if(request.readyState === 4){ // 请求响应都完毕了
-        if(request.status >= 200 && request.status < 300){
-          console.log('请求成功')
-          let string = request.responseText // 把符合JSON语法的字符串转换成JS对应的值
-          let object = window.JSON.parse(string)  // JSON.parse是浏览器提供的
-        }else if(request.status >= 400){
-          console.log('请求失败') 
-        }
+window.jQuery = function(nodeOrSelector){
+  let nodes = {}
+  nodes.addClass = function(){}
+  nodes.html = function(){}
+  return nodes
+}
+window.$ = window.jQuery
+
+window.jQuery.ajax = function({url, method, body, successFn, failFn, headers}){
+// window.jQuery.ajax = function(options){
+//   let url
+//   if(arguments.length === 1){ // 如果用户只传一个参数
+//     url = options.url
+//   }else if(arguments.length === 2){ // 如果用户传两个参数
+//     url = arguments[0]
+//     options = arguments[1]
+//   }
+//   let method = options.method
+//   let body = options.body
+//   let headers = options.headers
+//   let successFn = options.successFn
+//   let failFn = options.failFn
+// }
+  let request = new XMLHttpRequest()
+  request.open(method, url) // 初始化request
+  for(let key in headers){ // 设置请求头必须放在open和send之间
+    let value = headers[key]
+    request.setRequestHeader(key, value)
+  }
+  request.onreadystatechange = ()=>{
+    if(request.readyState === 4){ // 证明请求响应都完毕了
+      if(request.status >= 200 && request.status < 300){
+        successFn.call(undefined, request.responseText)
+      }else if(request.status >= 400){
+        failFn.call(undefinde, request)
       }
     }
+  }
+  request.send(body) // 设置请求第四部分（请求体）
+}
+
+function f1(responseText){}
+function f2(responseText){}
+
+myButton.addEventListener('click', (e)=>{
+  window.jQuery.ajax({ // 调用ajax函数
+    url: '/xxx',
+    method: 'get',
+    body: 'a=1&b=2',
+    headers: {
+      'Content-type': 'application/x-www-form-urlencoded',
+      'gang': '18'
+    },
+    successFn: (x)=>{
+      console.log('success')
+      f1.call(undefined, x)
+      f2.call(undefined, x)
+    },
+    failFn: (x)=>{
+      console.log('fail')
+      console.log(x)
+    }
+  })  
 })
