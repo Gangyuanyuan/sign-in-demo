b9b3b849579cc6eb1556c89b40c53c1195df260c
Gangyuan <296671396@qq.com>
2019-05-31 22:55:50 +0800

    Promise规则

--- main.js
+++ main.js
@@ -6,41 +6,26 @@ window.jQuery = function(nodeOrSelector){
 }
 window.$ = window.jQuery
 
-window.jQuery.ajax = function({url, method, body, successFn, failFn, headers}){
-// window.jQuery.ajax = function(options){
-//   let url
-//   if(arguments.length === 1){ // 如果用户只传一个参数
-//     url = options.url
-//   }else if(arguments.length === 2){ // 如果用户传两个参数
-//     url = arguments[0]
-//     options = arguments[1]
-//   }
-//   let method = options.method
-//   let body = options.body
-//   let headers = options.headers
-//   let successFn = options.successFn
-//   let failFn = options.failFn
-// }
-  let request = new XMLHttpRequest()
-  request.open(method, url) // 初始化request
-  for(let key in headers){ // 设置请求头必须放在open和send之间
-    let value = headers[key]
-    request.setRequestHeader(key, value)
-  }
-  request.onreadystatechange = ()=>{
-    if(request.readyState === 4){ // 证明请求响应都完毕了
-      if(request.status >= 200 && request.status < 300){
-        successFn.call(undefined, request.responseText)
-      }else if(request.status >= 400){
-        failFn.call(undefinde, request)
+window.jQuery.ajax = function({url, method, body, headers}){
+  return new Promise(function(resolve, reject){   // 全局属性 window.Promise
+    let request = new XMLHttpRequest()
+    request.open(method, url) // 初始化request
+    for(let key in headers){ // 设置请求头必须放在open和send之间
+      let value = headers[key]
+      request.setRequestHeader(key, value)
+    }
+    request.onreadystatechange = ()=>{
+      if(request.readyState === 4){ // 证明请求响应都完毕了
+        if(request.status >= 200 && request.status < 300){
+          resolve.call(undefined, request.responseText) // 成功则调用resolve
+        }else if(request.status >= 400){
+          reject.call(undefinde, request) // 失败则调用reject
+        }
       }
     }
-  }
-  request.send(body) // 设置请求第四部分（请求体）
+    request.send(body) // 设置请求第四部分（请求体）
+  }) 
 }
-
-function f1(responseText){}
-function f2(responseText){}
 
 myButton.addEventListener('click', (e)=>{
   window.jQuery.ajax({ // 调用ajax函数
@@ -50,15 +35,9 @@ myButton.addEventListener('click', (e)=>{
     headers: {
       'Content-type': 'application/x-www-form-urlencoded',
       'gang': '18'
-    },
-    successFn: (x)=>{
-      console.log('success')
-      f1.call(undefined, x)
-      f2.call(undefined, x)
-    },
-    failFn: (x)=>{
-      console.log('fail')
-      console.log(x)
     }
-  })  
+  }).then(
+    (text) => {console.log(text)},
+    (request) => {console.log(request)}
+  )
 })
