79d6d42d436ca6e9cbcacde133453618a477308c
Gangyuan <296671396@qq.com>
2019-06-28 22:30:41 +0800

    用户注册

--- server.js
+++ server.js
@@ -34,8 +34,34 @@ var server = http.createServer(function(request, response){
     response.write(string)
     response.end()
   }else if(path === '/sign_up' && method === 'POST'){
-    response.statusCode = 200
-    response.end()
+    readBody(request).then((body) => { // 读取第四部分
+      let strings = body.split('&') 
+      // strings = ['email=1', 'password=2', 'password_confirmation=3']
+      let hash = {}
+      strings.forEach((string) => { // string == 'email=1'
+        let parts = string.split('=') // ['email', '1']
+        let key = parts[0]
+        let value = parts[1]
+        hash[key] = value // hash['email'] = '1'
+      })
+      // hash = {'email':'1', 'password':'2', 'password_confirmation':'3'}
+      let {email, password, password_confirmation} = hash // ES6语法
+      if(email.indexOf('@') === -1){ // 若email中不含@
+        response.statusCode = 400
+        response.setHeader('Content-Type', 'application/json;charset=utf-8')
+        response.write(`{
+          "errors": {
+            "email": "invalid"
+          }
+        }`)
+      }else if(password !== password_confirmation){
+        response.statusCode = 400
+        response.write('password not match')
+      }else{
+        response.statusCode = 200
+      }
+      response.end()
+    })
   }else if(path === '/main.js'){
     let string = fs.readFileSync('./main.js', 'utf8')
     response.statusCode = 200
@@ -51,7 +77,7 @@ var server = http.createServer(function(request, response){
     {
       "note":{
         "to": "小谷",
-        "from": "方方",
+        "from": "小刚",
         "heading": "打招呼",
         "content": "hi"
       }
@@ -71,6 +97,18 @@ var server = http.createServer(function(request, response){
 
   /******** 代码结束，下面不要看 ************/
 })
+
+function readBody(request){
+  return new Promise((resolve, reject) => {
+    let body = [] // 请求体
+    request.on('data', (chunk) => { // 监听data
+      body.push(chunk)
+    }).on('end', () => {
+      body = Buffer.concat(body).toString() // 将所有data连接起来（因为一段一段上传的）
+      resolve(body)
+    })
+  })  
+}
 
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请在空中转体720度然后用电饭煲打开 http://localhost:' + port)
